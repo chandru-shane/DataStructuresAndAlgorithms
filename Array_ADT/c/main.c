9386371599ca36d5e2de15f6fbccbd9b4dad3272
chandru-shane <chandru2907@gmail.com>
2020-09-16 14:54:45 +0530

    Update Array Delete

--- Array_ADT/c/main.c
+++ Array_ADT/c/main.c
@@ -41,9 +41,26 @@ void Display(struct Array arr)
     }
 }
 
+int Delete(struct Array *arr, int index){
+    int i;
+    int x = 0;
+
+    if(index >= 0 && index <= arr->length){
+        x = arr->A[index];
+        for(i=index; i < arr->length-1;i++){
+            arr->A[i] = arr->A[i+1];
+        }
+        arr->length--;
+        return x;
+    }
+
+    return 0;
+}
+
 int main(){
     struct Array arr={{1,2,3,4,5}, 20, 5};
    
+    printf("%d ", Delete(&arr,4));
     Display(arr);
     return 0;
 }
