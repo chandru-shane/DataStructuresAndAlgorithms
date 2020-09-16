987516152043aedc456852ee22de54ea22ae7517
chandru-shane <chandru2907@gmail.com>
2020-09-16 14:41:33 +0530

    Updated Array insert

--- Array_ADT/c/main.c
+++ Array_ADT/c/main.c
@@ -10,6 +10,27 @@ struct Array
 
 };
 
+void Append(struct Array *arr, int x)
+{
+    if(arr->length < arr ->size)
+    {   
+      
+        arr->A[arr->length++] = x;
+        
+    }
+}
+
+void Insert(struct Array *arr, int index, int x){
+    int i;
+    if(index >= 0 && index <= arr-> length){
+        for( i=arr->length; i>index; i--){
+            arr->A[i] = arr->A[i-1];
+        }
+        arr->A[index] = x;
+        
+    }
+}
+
 
 void Display(struct Array arr)
 {
@@ -22,7 +43,7 @@ void Display(struct Array arr)
 
 int main(){
     struct Array arr={{1,2,3,4,5}, 20, 5};
-
+   
     Display(arr);
     return 0;
 }
