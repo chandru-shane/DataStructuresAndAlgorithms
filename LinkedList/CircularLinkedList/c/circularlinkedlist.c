ae81556cbd41bd3f0694ff56b940b4392381f5f6
chandru-shane <chandru2907@gmail.com>
2020-11-20 12:10:18 +0530

    Circular Linked List display

--- LinkedList/CircularLinkedList/c/circularlinkedlist.c
+++ LinkedList/CircularLinkedList/c/circularlinkedlist.c
@@ -4,20 +4,20 @@
 struct Node{
     int data;
     Node *next;
-}*first;
+}*head;
 
 void create(int A[], int n)
 {
     struct Node *last, *temp;
-    first = (struct Node *)malloc(sizeof(struct Node));
-    first->data = A[0];
-    first->next=first;
-    last = first;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = A[0];
+    head->next=head;
+    last = head;
     for(int i=1; i<n; i++)
     {
         temp = (struct Node *)malloc(sizeof(struct Node));
         temp->data= A[i];
-        temp->next = first;
+        temp->next = head;
         last->next=temp;
         last = temp;
         
@@ -26,9 +26,9 @@ void create(int A[], int n)
 
 void Display(struct Node *p){
     int flag = 1;
-    while(first!= p || flag)
+    while(head!= p || flag)
     {
-        if(first==p)
+        if(head==p)
         {
             flag=0;
         }
@@ -37,11 +37,32 @@ void Display(struct Node *p){
     }
 }
 
+void DOdisplay(struct Node *p)
+{   
+    printf("Running DOdisplay function\n");
+    do{
+        printf("%d ", p->data);
+        p = p->next;
+    }while(p!=head);
+}
+
+void Rdisplay(struct Node *h)
+{
+    static int flag = 1;
+    if(h!=head || flag)
+    {
+        flag = 0;
+        printf("%d ", h->data);
+        Rdisplay(h->next);
+    }
+    flag=0;
+}
+
 int main()
 {
     int A[] = {1,2,3,4,5};
     create(A, 5);
-    Display(first);
+    Rdisplay(head);
 
     return  0;
 }
