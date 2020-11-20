fb45186c1d6506fd491a42c46ab9b7e23ad23308
chandru-shane <chandru2907@gmail.com>
2020-11-20 10:44:50 +0530

    Matrix added

---
+++ Matrix/cpp/diagonal_matrix.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+class Diagonal{
+    private:
+        int *A;
+        int n;
+    public:
+        Diagonal(){
+            n=2;
+            A = new int[n];
+        }
+        Diagonal(int n){
+            this->n = n;
+            A = new int[n];
+        }
+        ~Diagonal(){
+            delete []A;
+        }
+        
+        void Set(int i, int j, int x);
+        int Get(int i, int j);
+        void Display();
+        int GetDimensions();
+};
+
+int Diagonal::GetDimensions()
+{
+    return n;
+}
+void Diagonal::Set(int i,int j, int x )
+{
+    if(i==j){
+        A[i-1]=x;
+    }
+    
+}
+
+int Diagonal::Get(int i, int j)
+{
+    if(i==j)
+    {
+        return A[i-1];
+    }
+}
+
+void Diagonal::Display()
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i==j)
+            {
+                cout<<A[i]<<" ";
+            }
+            else{
+                cout<<0<<" ";
+            }
+        
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    Diagonal d(4);
+    d.Set(1,1,1);
+    d.Set(2,2,4);
+    d.Set(3,3,9);
+    d.Set(4,4,16);
+    d.Display();
+    cout<<d.GetDimensions();
+    return 0;
+
+}
